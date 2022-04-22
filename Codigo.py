f5602ebd6d21750fceb487ac4858a2726f913b2c
Erick Holgado Bardales <33464722+EGholgado@users.noreply.github.com>
2022-04-22 00:54:21 -0500

    Update

--- Codigo.py
+++ Codigo.py
@@ -1,13 +1,54 @@
 import random
 
-print("===========================")
-print("   ¡Bienvenido al juego!   ")
-print("===========================")
+print("=========================================")
+print("          ¡Bienvenido al juego!          ")
+print("=========================================")
 
 print("El objetivo del juego es adivinar cual es el número"+"\n")
 
 inicio = int(1)
 final = int(10)
+
+print(f"El juego escoge el número {inicio} como el límite inicial y el número {final} como el límite final para escoger el número aleatorio a adivinar"
+          f", si desea cambiar los números correspondientes:"+"\n"+"Pulsar el número 0 para empezar a jugar"+"\n"+"Pulsar el valor 1 para cambiar los valores."+"\n")
+
+while True:
+    clave = input("Ingresa el número correspondiente a tu decisión: ")
+
+    while clave.isnumeric() == False:
+        print("El valor ingresado no es un número")
+        clave = input("Ingresa el número correspondiente a tu decisión: ")
+
+    clave = int(clave)
+    if clave == 0:
+        clave = int(clave)
+        print("¡Empecemos a jugar!")
+        break
+
+    elif clave == 1:
+        inicio = input("Ingresa el valor de inicio: ")
+
+        while inicio.isnumeric() == False:
+            print("El valor ingresado no es un número positivo")
+            inicio = input("Ingresa el valor de inicio: ")
+
+        inicio = int(inicio)
+
+        final = input("Ingresa el valor final: ")
+
+        while final.isnumeric() == False:
+            print("El valor ingresado no es un número positivo")
+            final = input("Ingresa el valor final: ")
+
+        final = int(final)
+
+        print(f"El juego escoge el número {inicio} como el límite inicial y el número {final} como el límite final para escoger el número aleatorio a adivinar"
+            f", si desea cambiar los números correspondientes:" + "\n" + "Pulsar el número 0 para empezar a jugar" + "\n" + "Pulsar el valor 1 para cambiar los valores." + "\n")
+
+    else:
+        print("El valor ingresado no es válido, solo ingresar 0 o 1")
+
+
 
 numero = random.randint(inicio,final)
 print(numero)
